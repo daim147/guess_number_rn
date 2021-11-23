9c6a33576de57b2d116c4fa63accc0fb7100f2b2
DaimSyed <daimshah784@gmail.com>
2021-11-23 14:34:34 +0500

    addded

---
+++ components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { View ,Text, StyleSheet} from 'react-native'
+import color from '../constants/color'
+
+const Header = (props) => {
+    return (
+        <View style={styles.header}>
+            <Text style={styles.headerTitle}>{props.title}</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    header:{
+        width: '100%',
+        height: 90,
+        paddingTop:36,
+        backgroundColor:color.primary,
+        alignItems:'center',
+        justifyContent:'center',
+    },
+    headerTitle:{
+        color: 'white',
+        fontSize:18,
+        fontWeight:'600',
+    }
+})
+
+export default Header
---
+++ screens/GameScreen.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+
+const generateRandomNumber = (min, max, exclude)=>{
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    const rndNum = Math.floor(Math.random() * (max - min)) + min
+    if(rndNum === exclude){
+       return generateRandomNumber(min,max,exclude)
+    }else{
+        return rndNum
+    }
+}
+const GameScreen = (props) => {
+    const [currentGuess, setCurrentGuess] = useState(generateRandomNumber())
+    return (
+        <View>
+            <Text>Game Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+export default GameScreen
